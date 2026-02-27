11eedb9b8b8fb93831d5b82bf40495617f0e1714
Marco Otto <ST10523648@myemeris.edu.za>
2026-02-27 11:37:39 +0200

    initial commit

---
+++ src/calculator/github/st10523648/CalculatorGithubST10523648.java
@@ -0,0 +1,7 @@
+package calculator.github.st10523648;
+
+public class CalculatorGithubST10523648 {
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
